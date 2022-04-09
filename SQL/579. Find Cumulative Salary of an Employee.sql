cc2ec6ad66aaba724df1c2287bd8d2c57091ab1c
w.cui <wycui68@gmail.com>
2022-04-09 12:57:56 -0700

    Create 579. Find Cumulative Salary of an Employee.sql

---
+++ SQL/579. Find Cumulative Salary of an Employee.sql
@@ -0,0 +1,15 @@
+# 每个id不包括最近的一个月，比如id=1，不包括8月份的
+
+select e1.id, e1.month, 
+e1.salary + ifnull(e2.salary, 0) + ifnull(e3.salary, 0) as Salary
+from Employee e1 left join Employee e2
+on e1.id = e2.id and e1.month = e2.month + 1
+left join Employee e3
+on e2.id = e3.id and e2.month = e3.month + 1
+where (e1.id, e1.month) not in 
+    (
+        select id, max(month)
+        from Employee
+        group by id
+    )
+order by e1.id, e1.month desc
