a401aeef6733c0eac6557a6b08014551642a4e65
w.cui <wycui68@gmail.com>
2022-10-22 15:58:08 -0500

    Update 534. Game Play Analysis III.sql

--- SQL/534. Game Play Analysis III.sql
+++ SQL/534. Game Play Analysis III.sql
@@ -1,3 +1,12 @@
+##### window function #####
+select player_id, 
+event_date, 
+sum(games_played) over(partition by player_id order by event_date rows between unbounded preceding and current row)
+as games_played_so_far
+from Activity
+
+
+##########################
 select a1.player_id, 
 a1.event_date, 
 sum(a2.games_played) as games_played_so_far
