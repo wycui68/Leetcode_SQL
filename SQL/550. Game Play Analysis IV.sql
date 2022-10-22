fd3eb63b955c754a1375f0887c7c51cf827fcdc0
w.cui <wycui68@gmail.com>
2022-10-22 15:58:46 -0500

    Update 550. Game Play Analysis IV.sql

--- SQL/550. Game Play Analysis IV.sql
+++ SQL/550. Game Play Analysis IV.sql
@@ -1,3 +1,22 @@
+##### window function #####
+with cet as 
+(
+select player_id, 
+event_date, 
+min(event_date) over(partition by player_id order by event_date) as first_login, 
+lead(event_date, 1) over(partition by player_id order by event_date) as lead1 
+from Activity
+)
+
+select round(
+count(distinct player_id) / (select count(distinct player_id) from Activity)
+, 2
+) as fraction 
+
+from cet
+where datediff(lead1, first_login) = 1
+
+##########################
 with t1 as 
 (
     select player_id, min(event_date) as first_log
