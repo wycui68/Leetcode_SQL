d828e23c164c1c37a4c91aec55b7de56ab68ee63
w.cui <wycui68@gmail.com>
2022-10-23 16:04:00 -0500

    Update 1212. Team Scores in Football Tournament.sql

--- SQL/1212. Team Scores in Football Tournament.sql
+++ SQL/1212. Team Scores in Football Tournament.sql
@@ -1,3 +1,33 @@
+with cet as 
+(
+select host_team as id1, 
+guest_team as id2, 
+host_goals as goal_1, 
+guest_goals as goal_2
+from Matches
+union all
+select guest_team as id1, 
+host_team as id2, 
+guest_goals as goal_1, 
+host_goals as goal_2
+from Matches
+)
+
+select t1.team_id, 
+t1.team_name, 
+ifnull(tot, 0) as num_points
+from Teams t1 left join 
+(
+select id1, 
+sum(case when goal_1 > goal_2 then 3 
+         when goal_1 = goal_2 then 1 
+         else 0 end) as tot
+from cet
+group by id1
+) t2 on t1.team_id = t2.id1
+order by num_points desc, team_id
+
+###########################################################
 with cet1 as 
 (
     select team_id, team_name, 
