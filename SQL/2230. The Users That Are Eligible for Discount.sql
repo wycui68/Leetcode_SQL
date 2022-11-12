9696988987dcbdd20379076487db8a62fd1aad1b
w.cui <wycui68@gmail.com>
2022-11-11 21:02:25 -0600

    Create 2230. The Users That Are Eligible for Discount.sql

---
+++ SQL/2230. The Users That Are Eligible for Discount.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE getUserIDs(startDate DATE, endDate DATE, minAmount INT)
+BEGIN
+	# Write your MySQL query statement below.
+	select distinct user_id
+    from Purchases
+    where time_stamp between startDate and endDate
+    and amount >= minAmount
+    order by 1;
+END
