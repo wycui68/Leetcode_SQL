98ca63460b99574146aa5e79903d5844466739ac
w.cui <wycui68@gmail.com>
2022-11-13 12:09:11 -0600

    Update 2298. Tasks Count in the Weekend.sql

--- SQL/2298. Tasks Count in the Weekend.sql
+++ SQL/2298. Tasks Count in the Weekend.sql
@@ -1,17 +1,4 @@
-with cet1 as 
-(
-    select task_id, submit_date, 
-    dayofweek(submit_date) as wkday
-    from Tasks
-), cet2 as
-(
-    select task_id, 
-    (case when wkday = 1  or wkday = 7 then 1 else 0 end) as weekend_cnt,
-    (case when wkday != 1 and wkday!= 7 then 1 else 0 end) as working_cnt
-    from cet1
-    group by task_id
-)
-
-select sum(weekend_cnt) as weekend_cnt, 
-sum(working_cnt) as working_cnt
-from cet2
+select 
+sum(case when dayofweek(submit_date) = 1 or dayofweek(submit_date) = 7 then 1 else 0 end) as weekend_cnt, 
+sum(case when dayofweek(submit_date) != 1 and dayofweek(submit_date) != 7 then 1 else 0 end) as working_cnt
+from Tasks
