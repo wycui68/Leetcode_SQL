043f8eaad560b834b742f392bc99daf8768e1a11
w.cui <wycui68@gmail.com>
2022-11-13 12:09:41 -0600

    Update 2308. Arrange Table by Gender.sql

--- SQL/2308. Arrange Table by Gender.sql
+++ SQL/2308. Arrange Table by Gender.sql
@@ -1,40 +1,13 @@
-with cet1 as
-    (
-        select user_id
-        , gender
-        , row_number() over(order by user_id) as rnk
-        , 0 as ordering
-        from Genders
-        where gender = 'female'
-    ), cet2 as 
-        (
-        select user_id
-        , gender
-        , row_number() over(order by user_id) as rnk
-        , 1 as ordering
-        from Genders
-        where gender = 'other'
-    ), cet3 as 
-    (
-        select user_id
-        , gender
-        , row_number() over(order by user_id) as rnk
-        , 2 as ordering
-        from Genders
-        where gender = 'male'
-    ), cet4 as
-    (
-        select *
-        from cet1
-        union all
-        select *
-        from cet2
-        union all
-        select *
-        from cet3
-    )
-    
-select user_id
-, gender
-from cet4
-order by rnk, ordering
+with cet as 
+(
+select user_id, 
+gender, 
+row_number() over(partition by gender order by user_id) as rnk1, 
+(case when gender = 'female' then 1 when gender = 'other' then 2 else 3 end) as rnk2
+from Genders
+)
+
+select user_id, 
+gender
+from cet
+order by rnk1, rnk2
