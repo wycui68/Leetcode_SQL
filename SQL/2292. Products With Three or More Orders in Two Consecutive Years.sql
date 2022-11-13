330d6d57e6cecbbbcd5dc10691a3460607275409
w.cui <wycui68@gmail.com>
2022-11-12 21:00:12 -0600

    Update 2292. Products With Three or More Orders in Two Consecutive Years.sql

--- SQL/2292. Products With Three or More Orders in Two Consecutive Years.sql
+++ SQL/2292. Products With Three or More Orders in Two Consecutive Years.sql
@@ -1,22 +1,23 @@
 #连续日期， 1321类似题
 # each year has >= 3 orders and do this in two consecutive years
-with cet1 as
+# lead/lag window function 
+
+with cet as 
 (
-    select product_id, year(purchase_date) as yr
-    from Orders
-    group by product_id, yr
-    having count(order_id) >= 3
-), cet2 as 
-(
-    select product_id, year(purchase_date) as yr
-    from Orders
-    group by product_id, yr
-    having count(order_id) >= 3
+select product_id, 
+count(distinct order_id) as cnt,
+year(purchase_date) as order_year
+from Orders
+group by product_id, year(purchase_date)
+having count(distinct order_id) >= 3
 )
 
-select distinct cet1.product_id
-from cet1, cet2
-where cet1.product_id = cet2.product_id 
-and (cet1.yr = cet2.yr or cet1.yr + 1 = cet2.yr)
-group by cet1.product_id, cet1.yr
-having count(cet2.yr) = 2 
+select distinct product_id
+from 
+(
+select product_id, 
+order_year, 
+lag(order_year) over(partition by product_id) as prev
+from cet
+) t
+where order_year - prev = 1
