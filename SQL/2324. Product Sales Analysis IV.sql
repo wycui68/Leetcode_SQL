96ec581cd2677f19d435d3adac4d0a349c8eec05
w.cui <wycui68@gmail.com>
2022-12-01 20:55:43 -0600

    Update 2324. Product Sales Analysis IV.sql

--- SQL/2324. Product Sales Analysis IV.sql
+++ SQL/2324. Product Sales Analysis IV.sql
@@ -1,18 +1,14 @@
-with cet1 as
+with cet as 
 (
-    select user_id, 
+    select t1.user_id, 
     t1.product_id, 
-    sum(price * quantity) as amt
+    dense_rank() over(partition by user_id order by sum(quantity * price) desc) as rnk 
     from Sales t1 left join Product t2
     on t1.product_id = t2.product_id
-    group by 1, 2
-), cet2 as 
-(
-    select user_id, product_id, 
-    dense_rank() over(partition by user_id order by amt desc) as rnk 
-    from cet1
+    group by t1.user_id, t2.product_id
 )
 
-select user_id, product_id
-from cet2
+select user_id, 
+product_id
+from cet
 where rnk = 1
