49460ae8536a4f706b83371ebacc38ac30536f89
w.cui <wycui68@gmail.com>
2023-04-09 15:25:54 -0700

    Update 615. Average Salary: Departments VS Company.sql

--- SQL/615. Average Salary: Departments VS Company.sql
+++ SQL/615. Average Salary: Departments VS Company.sql
@@ -1,22 +1,23 @@
-with company as 
-    (
-        select date_format(pay_date, '%Y-%m') as pay_month, 
-               avg(amount) as company_avg
-        from Salary
-        group by pay_month
-    ), dep as 
-    (
-    select date_format(pay_date, '%Y-%m') as pay_month,
-           department_id, 
-           avg(amount) as dep_avg
-    from Salary s left join Employee e 
-    on s.employee_id = e.employee_id
-    group by pay_month, department_id
-    )
-    
-select c.pay_month, d.department_id, 
-(case when dep_avg < company_avg then 'lower'
-     when dep_avg > company_avg then 'higher'
-     else 'same' end) as comparison
-from company c, dep d
-where c.pay_month = d.pay_month
+### window function ###
+with cet as 
+(
+  select t1.employee_id, 
+  amount, 
+  date_format(pay_date, "%Y-%m") as pay_month, 
+  t2.department_id
+  from Salary t1 left join Employee t2 
+  on t1.employee_id = t2.employee_id
+), base as 
+(
+  select distinct pay_month, department_id, 
+  avg(amount) over(partition by pay_month) as avg_company, 
+  avg(amount) over(partition by pay_month, department_id) as avg_dept
+  from cet
+)
+
+select pay_month, 
+department_id, 
+case when avg_company = avg_dept then 'same' 
+     when avg_dept > avg_company then 'higher'
+     else 'lower' end as comparison
+from base
