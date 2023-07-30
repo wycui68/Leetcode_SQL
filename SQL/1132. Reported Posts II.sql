252b2b5a6c5d00f5535b5ca21f16f3edc5aab0c4
w.cui <wycui68@gmail.com>
2023-07-30 13:51:09 -0700

    Update 1132. Reported Posts II.sql

--- SQL/1132. Reported Posts II.sql
+++ SQL/1132. Reported Posts II.sql
@@ -1,22 +1,18 @@
-with cet1 as 
+with table1 as 
 (
-    select distinct action_date, post_id
-    from Actions
-    where action = 'report' and extra = 'spam'
-), cet2 as 
-(
-    select cet1.*
-    from cet1 join Removals
-    on cet1.post_id = Removals.post_id
-), cet3 as
-(
-    select distinct cet1.action_date, 
-    100 * count(cet2.post_id)/count(cet1.post_id) as pct
-    from cet1 left join cet2
-    on cet1.action_date = cet2.action_date and cet1.post_id = cet2.post_id
-    group by cet1.action_date
+    select t1.action_date, 
+    100 * count(distinct t2.post_id) / count(distinct t1.post_id) as pct
+    from 
+    (
+        select post_id, 
+        action_date
+        from Actions 
+        where  extra = 'spam'
+    ) t1 left join Removals t2 
+    on t1.post_id = t2.post_id
+    group by t1.action_date
 )
 
-select round(avg(pct), 2) as average_daily_percent
-from cet3
+select round(sum(pct) / count(distinct action_date), 2) as average_daily_percent
+from table1
 
