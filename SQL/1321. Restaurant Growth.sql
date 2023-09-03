9ced0cef9c4b7a0355cc5745aa50ea71c522ed1d
w.cui <wycui68@gmail.com>
2023-09-03 15:35:36 -0700

    Update 1321. Restaurant Growth.sql

--- SQL/1321. Restaurant Growth.sql
+++ SQL/1321. Restaurant Growth.sql
@@ -19,3 +19,22 @@ where datediff(cet1.visited_on, cet2.visited_on) >= 0 and
 datediff(cet1.visited_on, cet2.visited_on) < 7
 group by cet1.visited_on
 having count(*) = 7
+
+# window function 
+# rows between -> based on the row number 
+# range between -> based on the row values -> this problem req. 6 previous days
+# min() -> output only one row w/ min of the table
+# min() over() -> output each window w/min min of the table 
+
+select visited_on, 
+amount, 
+round(amount/7, 2) as average_amount
+from 
+(
+select distinct visited_on, 
+sum(amount) over(order by visited_on range between interval 6 day preceding and current row) as amount, 
+min(visited_on) over() as min_dt
+from Customer
+) tmp
+where datediff(visited_on, min_dt) >= 6
+order by visited_on
