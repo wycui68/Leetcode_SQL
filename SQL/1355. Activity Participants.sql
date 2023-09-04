40a60303d911c3beee645321af21980bd847fbb6
w.cui <wycui68@gmail.com>
2023-09-04 11:34:24 -0700

    Update 1355. Activity Participants.sql

--- SQL/1355. Activity Participants.sql
+++ SQL/1355. Activity Participants.sql
@@ -13,3 +13,20 @@ and count(*) != (select count(*)
                     order by count(*) desc
                     limit 1
                     )
+# window function 
+with table1 as 
+(
+    select activity, 
+    count(*) over(partition by activity) as cnt
+    from Friends
+), table2 as 
+(
+    select activity, 
+    rank() over(order by cnt) as rnk_asc, 
+    rank() over(order by cnt desc) as rnk_desc
+    from table1
+)
+
+select distinct activity
+from table2
+where rnk_asc !=1 and rnk_desc != 1
