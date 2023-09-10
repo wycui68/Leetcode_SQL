ae7313a09e0c4bde1e7fe870ce1e5ce582986b93
w.cui <wycui68@gmail.com>
2023-09-10 12:38:58 -0700

    Update 1445. Apples & Oranges.sql

--- SQL/1445. Apples & Oranges.sql
+++ SQL/1445. Apples & Oranges.sql
@@ -14,3 +14,11 @@ select cet1.sale_date, cet1.sold_num - cet2.sold_num as diff
 from cet1, cet2
 where cet1.sale_date = cet2.sale_date
 order by 1
+
+#case when 
+select sale_date, 
+sum(case when fruit = "apples" then sold_num
+     when fruit = "oranges" then -sold_num end) as diff
+from Sales
+group by sale_date
+order by 1
