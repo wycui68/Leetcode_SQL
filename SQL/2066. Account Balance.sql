9d905e27b64beb66cae9828ec3109d6b0b3f99f2
w.cui <wycui68@gmail.com>
2024-03-23 22:45:09 -0700

    Update 2066. Account Balance.sql

--- SQL/2066. Account Balance.sql
+++ SQL/2066. Account Balance.sql
@@ -5,3 +5,13 @@ sum(case when type = 'Deposit' then amount
          else 0 end) over(partition by account_id order by day) as balance
 from Transactions
 order by account_id, day
+
+#########################################################################
+# Write your MySQL query statement below
+select t1.account_id, 
+t1.day, 
+sum(case when t2.type="Deposit" then t2.amount 
+         when t2.type="Withdraw" then -t2.amount end) as balance
+from Transactions t1 left join Transactions t2 
+on t1.account_id = t2.account_id 
+and t1.day >= t2.day
