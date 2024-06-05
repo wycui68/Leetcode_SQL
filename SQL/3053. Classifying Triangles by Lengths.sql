5c4172ed406ae33a71da15e9043892cfb2d3a0c7
w.cui <wycui68@gmail.com>
2024-06-04 21:51:02 -0700

    Create 3053. Classifying Triangles by Lengths.sql

---
+++ SQL/3053. Classifying Triangles by Lengths.sql
@@ -0,0 +1,8 @@
+select case 
+when A+B <= C or A+C <= B or B+C<=A then 'Not A Triangle'
+when A=B and B=C then 'Equilateral'
+when A = B or A = C or B = C then 'Isosceles'
+
+
+else 'Scalene' end as triangle_type
+from Triangles
