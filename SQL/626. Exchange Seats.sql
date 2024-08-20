4b20d453e24a61bdfa09e4f4cfd92cf313afb999
w.cui <wycui68@gmail.com>
2024-08-20 12:22:36 -0700

    Update 626. Exchange Seats.sql

--- SQL/626. Exchange Seats.sql
+++ SQL/626. Exchange Seats.sql
@@ -1,18 +1,9 @@
-# if id is odd but not the last one -> new == id+1
-# if id is odd but the last ont -> new == id
-# if id the even -> id -1
-with cet1 as (
-    select *
-    from Seat, 
-        (
-            select count(distinct id) as cnt
-            from Seat
-        ) t
-)
-
-select case when id = cnt and id % 2 = 1 then id
-            when id != cnt and id % 2 = 1 then id + 1
-            else id - 1 end as id, 
-       student
-from cet1 
+select (
+    case when id = (select max(id) from Seat) and id % 2 = 1 then id 
+    when id % 2 = 1 then id + 1
+    when id % 2 = 0 then id - 1
+    end 
+) as id, 
+student
+from Seat
 order by id; 
