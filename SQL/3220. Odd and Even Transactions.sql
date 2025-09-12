b218e43547b6e44de8c939c073891616349ea19a
w.cui <wycui68@gmail.com>
2025-09-11 22:06:54 -0700

    Create 3220. Odd and Even Transactions.sql

---
+++ SQL/3220. Odd and Even Transactions.sql
@@ -0,0 +1,7 @@
+select
+transaction_date, 
+sum(case when amount % 2 = 1 then amount else 0 end) as odd_sum, 
+sum(case when amount % 2 = 0 then amount else 0 end) as even_sum
+from transactions
+group by transaction_date
+order by 1;
