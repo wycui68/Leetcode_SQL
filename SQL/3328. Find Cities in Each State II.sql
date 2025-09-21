a7a588df3582f3d8207ccd18ba45915ad3b13703
w.cui <wycui68@gmail.com>
2025-09-20 22:39:27 -0700

    Create 3328. Find Cities in Each State II.sql

---
+++ SQL/3328. Find Cities in Each State II.sql
@@ -0,0 +1,8 @@
+SELECT
+  state,
+  GROUP_CONCAT(city ORDER BY city ASC separator ', ') AS cities,
+  SUM(IF(LEFT(city, 1) = LEFT(state, 1), 1, 0)) AS matching_letter_count
+FROM cities
+GROUP BY 1
+HAVING COUNT(city) > 2 AND matching_letter_count > 0
+ORDER BY matching_letter_count DESC, state ASC
