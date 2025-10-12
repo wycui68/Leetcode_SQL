1ca90b3d3b4c519e35fd1701dc859b92b2f819cc
w.cui <wycui68@gmail.com>
2025-10-11 23:09:00 -0700

    Update 3601. Find Drivers with Improved Fuel Efficiency.sql

--- SQL/3601. Find Drivers with Improved Fuel Efficiency.sql
+++ SQL/3601. Find Drivers with Improved Fuel Efficiency.sql
@@ -1,30 +1,32 @@
-WITH
-first_half AS(
-    SELECT 
-        driver_id
-        ,AVG(distance_km/fuel_consumed) AS first_half_avg
-    FROM trips
-    WHERE MONTH(trip_date) BETWEEN 1 AND 6
-    GROUP BY driver_id
+with first_half as 
+(
+    select
+    driver_id, 
+    avg(distance_km/fuel_consumed) as first_half_avg
+    from trips
+    where month(trip_date) between 1 and 6
+    group by 1
 )
-,second_half AS(
-    SELECT
-        driver_id
-        ,AVG(distance_km/fuel_consumed) AS second_half_avg
-    FROM trips
-    WHERE MONTH(trip_date) BETWEEN 7 AND 12
-    GROUP BY driver_id
+, second_half as 
+(
+    select
+    driver_id, 
+    avg(distance_km/fuel_consumed) as second_half_avg
+    from trips
+    where month(trip_date) between 7 and 12
+    group by 1
 )
-SELECT 
-    d.driver_id
-    ,d.driver_name
-    ,ROUND(f.first_half_avg,2) AS first_half_avg
-    ,ROUND(s.second_half_avg,2) AS second_half_avg
-    ,ROUND(s.second_half_avg-f.first_half_avg,2) AS efficiency_improvement
-FROM drivers d
-JOIN first_half f
-    ON d.driver_id=f.driver_id
-JOIN second_half s
-    ON d.driver_id=s.driver_id
-WHERE f.first_half_avg<s.second_half_avg
-ORDER BY efficiency_improvement DESC, d.driver_name ASC
+
+select
+t1.driver_id, 
+t1.driver_name, 
+round(t2.first_half_avg, 2) as first_half_avg, 
+round(t3.second_half_avg, 2) as second_half_avg, 
+round(second_half_avg - first_half_avg, 2) as efficiency_improvement
+from drivers t1 join first_half t2 
+on t1.driver_id = t2.driver_id 
+join second_half t3 
+on t2.driver_id = t3.driver_id
+where second_half_avg > first_half_avg
+order by 5 desc, 2
+
